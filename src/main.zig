0226fc9f955c5975990e3a764f9746bd2117c744
dan <dan@pop-os.localdomain>
2023-04-01 19:51:20 -0500

    repl

--- src/main.zig
+++ src/main.zig
@@ -15,9 +15,22 @@ pub const Token = struct {
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    
+    const stdin = std.io.getStdIn().reader();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    while (true) {
+        stdin.readUntilDelimiterArrayList(&line, '\n', std.math.maxInt(usize)) catch |e| switch(e) {
+            error.EndOfStream => break,
+            else => return e,
+        };
+        
+    }
 
 
 
-    try stdout.print("Hello, world!\n", .{});
+    try stdout.print("Goodbye cruelworld... \n", .{});
 }
