453a9c22cee329c3d16d2cf33f261f2d81bbed18
dan <lordaniel24@gmail.com>
2023-04-10 15:41:00 -0500

    more i guess

--- src/main.zig
+++ src/main.zig
@@ -16,19 +16,42 @@ pub const Token = struct {
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
+    _ = stdin;
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     var line = std.ArrayList(u8).init(allocator);
+
+    var tokenlist = []const u8;
+    
     defer line.deinit();
 
-    while (true) {
-        stdin.readUntilDelimiterArrayList(&line, '\n', std.math.maxInt(usize)) catch |e| switch(e) {
-            error.EndOfStream => break,
-            else => return e,
-        };
-        
+    var start = line;
+    var end = start;
+
+
+    while (end < line + @as(u64, line.len)) {
+        if (end[0] == ' ' or end[0] == '\t' or end[0] == '\n'
+        or end[0] == '\r') {
+            if (end != start) {
+                const token = try std.mem.slice(allocator, start, end - start);
+                tokenlist |= token;
+            }
+            start = end + 1;
+        } 
+        end += 1;
     }
+
+    if (end != start) {
+        const token = try std.mem.slice(allocator, start, end - start);
+        tokenlist |= token;
+    }
+
+    for (tokenlist) |t| {
+        std.debug.print("{s}\n", .{t});
+    }
+        
+
 
 
 
